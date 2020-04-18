6164dc95ce19cf60e2a14e9cd25936868a57007b
Nico Prat <nicooprat@gmail.com>
2020-04-18 16:23:57 +0200

    Add tags and width props to customize rendering

--- packages/vue-columns/src/Columns.vue
+++ packages/vue-columns/src/Columns.vue
@@ -6,28 +6,39 @@ export default {
       type: Number,
       default: 1,
     },
+    tags: {
+      type: String,
+      default: 'div>div',
+    },
+    width: {
+      type: Function,
+      default: ({ current, total }) => `${100 / total}%`,
+    },
   },
   render (h, ctx) {
     const vnodes = ctx.scopedSlots.default && ctx.scopedSlots.default()
     if (!vnodes) {
       return
     }
+    const [row = 'div', column = 'div'] = ctx.props.tags.split('>')
     // Render children columns and dispatch children in them from left to right
     return h(
-      'div',
+      row,
       {
         ...ctx.data,
-        style: 'display: flex; word-break: break-word;',
+        style: 'display: flex;',
       },
-      Array.from(Array(ctx.props.columns)).map((col, i) =>
-        h(
-          'div',
+      Array.from(Array(ctx.props.columns)).map((col, i) => {
+        const width = ctx.props.width({ current: i, total: ctx.props.columns})
+        debugger
+        return h(
+          column,
           {
-            style: `flex: 0 0 ${100 / ctx.props.columns}%; width: ${100 / ctx.props.columns}%;`,
+            style: `flex: 0 0 ${width}; width: ${width};`,
           },
           vnodes.filter((vnode, j) => j % ctx.props.columns === i),
-        ),
-      ),
+        )
+      }),
     )
   },
 }
